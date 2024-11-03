ab43c40a3e066f5f50b9b31afad2c385f946677d
kbaduwal <khakendra1999@gmail.com>
2024-11-03 16:56:25 +0530

    This is our first component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet,Text,View, Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
+import React from 'react';
+
+
+// const product = products[0];
+
+const ProductListItem = ({product}) => {  
+  return(
+    <View style={styles.container}>
+    <Image source={{uri:product.image}}style={styles.image} />
+   <Text style={styles.title}>{product.name}</Text>
+   <Text style={styles.price}> ${product.price}</Text>
+  </View>
+  );
+};
+
+export default ProductListItem;
+
+
+const styles = StyleSheet.create({
+  
+  container: {
+    backgroundColor:'white',
+    padding:10,
+    borderRadius:20,
+    
+  },
+  image:{
+    width:'100%',
+    aspectRatio:1,
+
+  },
+  
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  title:{
+    fontSize:20,
+    fontWeight:'600',
+    marginVertical:10
+  },
+  price:{
+    color: Colors.light.tint,
+    fontWeight: 'bold'
+  }
+});
