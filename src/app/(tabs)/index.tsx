0fce042deabfed06b2f4064104d82ba613021295
kbaduwal <khakendra1999@gmail.com>
2024-11-03 21:05:34 +0530

    Configuring Expo Router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,9 @@
-import { View } from 'react-native';
-import products from '@/assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
+import { Redirect } from "expo-router";
 
-export default function MenuScreen() {
-  return (
-   <View>
-    <ProductListItem product={products[5]} />
-    <ProductListItem product={products[1]}/>
-   </View>
-   
-  );
+const TabIndex = () => {
+    return <Redirect href="/menu" />;
 }
+
+export default TabIndex;
+
+
