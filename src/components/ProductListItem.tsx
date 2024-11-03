89428a3dd9130ebd909d7e33a31fd53784b34c3a
kbaduwal <khakendra1999@gmail.com>
2024-11-03 17:23:01 +0530

    Type Safe component

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,14 +1,18 @@
 import { StyleSheet,Text,View, Image } from 'react-native';
 import Colors from '@/src/constants/Colors';
-import React from 'react';
+import { Product } from '../providers/types';
 
+export const defaultPizzaImage=
+'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
-// const product = products[0];
+type ProductListItemProps ={
+    product:Product;
+}
 
-const ProductListItem = ({product}) => {  
+const ProductListItem = ({product}:ProductListItemProps) => {  
   return(
     <View style={styles.container}>
-    <Image source={{uri:product.image}}style={styles.image} />
+    <Image source={{uri:product.image || defaultPizzaImage}}style={styles.image} />
    <Text style={styles.title}>{product.name}</Text>
    <Text style={styles.price}> ${product.price}</Text>
   </View>
