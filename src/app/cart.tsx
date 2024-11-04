7dc9d32dfea737ffe7a911a40bc6f8464741a5e6
kbaduwal <khakendra1999@gmail.com>
2024-11-04 20:22:36 +0530

    checkout price sum

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,18 +2,24 @@ import { View, Text,Platform, FlatList } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
 import { useCart } from '../poviders/CartProvider';
 import CartListItem from '../components/CartListItem';
+import Button from '../components/Button';
 
 
 const CartScreen = () => {
-  const {items} = useCart();
+  const {items, total} = useCart();
 
 
   return (
-    <View>
+    <View style={{padding:10}}>
       <FlatList data={items} 
-      renderItem={({item}) => <CartListItem cartItem={item} />}
-      contentContainerStyle={{padding:10,gap:10}}
-         />
+        renderItem={({item}) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{gap:10}}
+      />
+
+      <Text style={{marginTop:20, fontSize:20, fontWeight:'500'}}>
+        Total: ${total}
+      </Text>
+      <Button text="checkout"/>
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
